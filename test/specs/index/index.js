200c7f95cc5b41621916be8f140c49ad471218a1
Ivan Chashkin <chashkiniyu@sibur.ru>
2019-08-15 23:53:28 +0300

    master; done;

--- test/specs/index/index.js
+++ test/specs/index/index.js
@@ -5,15 +5,18 @@ describe('Payment check', () => {
         browser.url('/');
         let frame = $('#widget-container iframe');
         browser.switchToFrame(frame);
+        // browser.execute(() => { window.location.href = 'https://paymentpage.ecommpay.com/payment?is_test=1&payment_amount=1200&payment_currency=USD&payment_description=ECT_TEST_PROD_TEST_2012214&payment_id=ECT_TEST_PROD_TEST_2012214&project_id=1124&signature=po%2B%2BILNwFZWJgtbmulC2A%2FabGgFY6OgRE11MfcXQ70Vxlb0EWODfEKCurA8Xoz%2BVD4jCAYUbqwLguKYTZHshEQ%3D%3D'; });
     });
-    it('should have wait payment', () => {
+    it('should have copy address', () => {
         const $bitcoin = $('.ps__item[data-payment-method-code="crypto"]*=Bitcoin');
-        $bitcoin.waitForDisplayed(10000);
+        $bitcoin.waitForDisplayed(30000);
         $bitcoin.click();
 
-        const $paymentResult = $('.payment-result');
-        $paymentResult.waitForExist(60000);
-        const $paymentResultFail = $('.payment-result.payment-result_fail');
-        assert.equal($paymentResultFail.isDisplayed(), false, 'Payment has expired');
+        const $screenLoading = $('#screen-loading #preloader_wrapper');
+        $screenLoading.waitForDisplayed(20000);
+        $screenLoading.waitForDisplayed(60000, true);
+
+        const $copyAddress = $('#copy_address');
+        assert.equal($copyAddress.isDisplayed(), true, 'Copy address not visible');
     });
 });
